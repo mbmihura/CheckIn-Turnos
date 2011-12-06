5756a3933e7505b42dae9707feaf4702c8a2e40a
Martin Mihura <mbmihura@gmail.com>
2011-12-06 09:20:44 -0300

    Agrega clase dbBar con firmas de sus metodos

---
+++ db -Turnos/dbBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace db__Turnos
+{
+    public class TransDb
+    {
+        SqlConnection _conexion = new SqlConnection(new Properties.Settings().Bar_TurnosConnectionString);
+        
+        SqlCommand _comando = new SqlCommand();
+
+        public TransDb()
+        {
+            _comando.Connection = _conexion;
+        }
+
+        public SqlDataReader getTurnosAbiertos()
+        {
+            return new SqlDataReader();
+        }
+
+        public void AbrirTurnos()
+        {}
+
+        public void CerrarTurno()
+        {}
+
+        public void CambiarContraseña()
+        {}
+    }
+}
