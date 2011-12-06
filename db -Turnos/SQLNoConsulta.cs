9fa232920b9c51a940dd96a0497db5807b561e15
Martin Mihura <mbmihura@gmail.com>
2011-12-06 11:50:12 -0300

    Agrega clases AutoGeneratedDataTable y SQLNoConsulta, para comunicarse con la estructura de la db.

---
+++ db -Turnos/SQLNoConsulta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace db__Turnos
+{
+    /// <summary>Objeto que ejecuta una sentencia SQL que no devuelve datos de por si (SqlCommand.ExecuteNonQuery).</summary>
+    class SQLNoConsulta
+    {
+        private string _sentencia;
+        public SQLNoConsulta(string sentencia)
+        {
+            _sentencia = sentencia;
+        }
+        public int Ejecutar()
+        {
+            using (SqlConnection conexion = new SqlConnection(new Properties.Settings().Bar_TurnosConnectionString))
+            {
+                SqlCommand comandoSql = new SqlCommand(_sentencia, conexion);
+                try
+                {
+                    conexion.Open();
+                    return comandoSql.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+            }
+        }
+    }
+}
+
